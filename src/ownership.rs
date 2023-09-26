34bb6e3fa218cb92b82b34ab7a4afe4caf8e310f
Alessio Saltarin <alessiosaltarin@gmail.com>
2023-09-26 10:02:11 +0200

    Better readability in taking ownership

--- src/ownership.rs
+++ src/ownership.rs
@@ -7,7 +7,7 @@
 pub(crate) fn ownerhip_demo() {
 
     let one_string = String::from("One String");
-    let mut result: usize = take_ownership(one_string);
+    let mut result: usize = get_len_taking_ownership(one_string);
     println!("1. The 1 string size is = {}", result);
 
     // No! This violates OWNERSHIP rules
@@ -15,28 +15,28 @@ pub(crate) fn ownerhip_demo() {
 
     // This is OK
     let two_string = String::from("Two String");
-    let results = return_ownership(two_string);
+    let results = get_len_returning_ownership(two_string);
     println!("2. The 2 string size is = {}", results.0);
 
     // This is ALSO OK
     let three_string = String::from("Three String");
-    result = operates_on_reference(&three_string);
+    result = get_len_with_reference(&three_string);
     println!("3. The 3 string size is = {}", result);
 
 }
 
-fn take_ownership(a_string: String) -> usize {
+fn get_len_taking_ownership(a_string: String) -> usize {
     // This function TAKES OWNERSHIP of the a_string
     let string_len = a_string.len();
     string_len
 }
 
-fn return_ownership(a_string: String) -> (usize, String) {
+fn get_len_returning_ownership(a_string: String) -> (usize, String) {
     let string_len = a_string.len();
     (string_len, a_string)
 }
 
-fn operates_on_reference(a_string_reference: &String) -> usize {
+fn get_len_with_reference(a_string_reference: &String) -> usize {
     let string_len = a_string_reference.len();
     string_len
 }
@@ -48,7 +48,7 @@ mod tests {
     #[test]
     fn string_len_test() {
         let a_string = String::from("A test string");
-        let string_size = operates_on_reference(&a_string);
+        let string_size = get_len_with_reference(&a_string);
         assert_eq!(13, string_size);
     }
 }
